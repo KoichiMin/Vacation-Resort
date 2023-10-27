4e733c9ffce7b7cc4f3f364c6fb6a449bd965990
KoichiMin <koichidminami@gmail.com>
2023-10-27 12:15:52 -0400

    finished displaying cost of stay

---
+++ scripts/overnightCostPage.js
@@ -0,0 +1,121 @@
+"use strict"
+
+window.onload = function(){
+    const reservationBtn = document.getElementById("reservationBtn");
+    reservationBtn.onclick = function(event){
+        event.preventDefault(); 
+        estimateCost();
+    }
+}
+
+function estimateCost(){
+    const roomTypes = {
+        "queen": {
+        type: "queen",
+        capacity: 5,
+        "offSeason": 150,
+        "inSeason": 250,
+        },
+        "king": {
+        type: "king",
+        capacity: 2,
+        offSeason: 150,
+        "inSeason": 250,
+        },
+        "twoBedroom": {
+        type: "twoBedroom",
+        capacity: 6,
+        "offSeason": 210,
+        "inSeason": 350,
+        },
+    };
+
+    const months = ["January", "February", "March", "April",
+    "May", "June", "July", "August", "September",
+    "October", "November", "December"];
+
+
+    // find the bedside option chosen by user 
+    const radioBtns = document.querySelectorAll('input[name="bedside"]');
+    let bedsideOption  = Array.from(radioBtns).find((radio)=>{
+        return  radio.checked; 
+    })
+    bedsideOption = bedsideOption.value
+    // console.log(typeof bedsideOption, roomTypes[bedsideOption].capacity)
+    
+    // calculate total amount of guests
+    const numAdults = Number(document.getElementById("numAdults").value);
+    const numKids = Number( document.getElementById("numKids").value);
+    const totalGuests = numAdults + numKids;
+    // console.log(totalGuests)
+
+    // send message if total amount of guest is higher than capacity of bedroom
+    if(roomTypes[bedsideOption].capacity < totalGuests){
+        const messageDiv = document.getElementById("messageDiv");
+        messageDiv.innerText = "The room you selected will not hold your party"
+        const costInfo = document.getElementById("costInfo");
+        costInfo.style.display = "none";
+    } else{
+        const messageDiv = document.getElementById("messageDiv");
+        messageDiv.innerText = "";
+        const costInfo = document.getElementById("costInfo");
+        costInfo.style.display = "block";
+        
+        // find month of checking
+        const checkIn =  document.getElementById("checkIn");
+        const dateString = checkIn.value;
+        const date = new Date(dateString);
+        const findMonth = months[date.getMonth()];
+        // console.log( findMonth);
+
+        // find out if month is considered in Season or out of Season
+        const inSeasonDate = ["June", "July", "August"];
+        let season;
+        if(inSeasonDate.includes(findMonth)){
+            season = "inSeason"
+        } else{
+            season = "offSeason"
+        }
+        // console.log(season)
+
+        // find number of Days user staying
+        const numDays = Number(document.getElementById("numDays").value);
+
+        // original cost 
+        const originalCost = numDays * roomTypes[bedsideOption][season]
+        console.log(originalCost)
+
+        // find discounts
+        let discountAmount;
+        let discountDisplay = 0
+        const radioBtns = document.querySelectorAll('input[name="discount"]');
+        let discountOption  = Array.from(radioBtns).find((radio)=>{
+            return  radio.checked; 
+        })
+        discountOption = discountOption.value
+        if(discountOption == "aaa"){
+            discountAmount = 0.1
+            discountDisplay = originalCost * discountAmount
+        }else if(discountOption === "military"){
+            discountAmount = 0.2
+            discountDisplay = originalCost * discountAmount
+        }
+
+        // discounted room Cost
+        const discountedRoomCost = originalCost - discountDisplay
+
+        //  taxes
+        const taxes = discountedRoomCost * 0.12
+
+
+        //final cost 
+        const finalCost = taxes + discountedRoomCost
+        console.log(finalCost)
+        document.getElementById("originalRoomCost").innerText = "$" + originalCost.toFixed(2);
+        document.getElementById("discountDisplay").innerText = "$" + discountDisplay.toFixed(2);
+        document.getElementById("discountedRoomCost").innerText = "$" + discountedRoomCost.toFixed(2)
+        document.getElementById("taxes").innerText = "$" + taxes.toFixed(2)
+        document.getElementById("finalCost").innerText = "$" + finalCost.toFixed(2);
+
+    }
+}
